0c12566fa954a7e9b41902f572c00c6a50516af2
Ulises Roldan <leonel.matamoros21@gmail.com>
2025-10-04 22:30:20 -0600

    Cambios 04-10-2025

--- app/src/main/java/com/example/uthp3mv1/ActivityDash.java
+++ app/src/main/java/com/example/uthp3mv1/ActivityDash.java
@@ -4,17 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityDash extends AppCompatActivity {
 
-    Button btnagregar, btncontacto, btncombo;
+    Button btnagregar, btncontactos, btncombo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +19,7 @@ public class ActivityDash extends AppCompatActivity {
         setContentView(R.layout.activity_dash);
 
         btnagregar = (Button)  findViewById(R.id.btnagregar);
-        btncontacto = (Button)  findViewById(R.id.btncontacto);
+        btncontactos = (Button)  findViewById(R.id.btncontactos);
         btncombo = (Button)  findViewById(R.id.btncombo);
 
         btnagregar.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +27,22 @@ public class ActivityDash extends AppCompatActivity {
             public void onClick(View view) {
 
                 Intent intent = new Intent(ActivityDash.this, MainActivity.class);
-                startActivity();
+                startActivity(intent);
+            }
+        });
+
+        btncombo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        btncontactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
 
             }
         });
