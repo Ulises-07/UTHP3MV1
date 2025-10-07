b5a9562a64cfd04259f2aece2f1f2da45ba436eb
Ulises Roldan <leonel.matamoros21@gmail.com>
2025-10-06 21:21:22 -0600

    Cambios 06-10-2025, permiso de camara.

--- app/src/main/java/com/example/uthp3mv1/MainActivity.java
+++ app/src/main/java/com/example/uthp3mv1/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.uthp3mv1;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.uthp3mv1.configuraciones.SQLiteConexion;
+import com.example.uthp3mv1.configuraciones.Transacciones;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText nombres, apellidos, edad;
-    Button btnprocesar;
+    EditText nombres, apellidos, edad, correo;
+    ImageView imageView;
+    Button btnprocesar, btnfoto;
 
+    private static final int PERMISO_CAMARA = 101;
+
+    ActivityResultLauncher<Intent> tomarFotoLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +42,103 @@ public class MainActivity extends AppCompatActivity {
         nombres = (EditText) findViewById(R.id.nombres);
         apellidos = (EditText) findViewById(R.id.apellidos);
         edad = (EditText) findViewById(R.id.edad);
+        correo = (EditText) findViewById(R.id.correo);
+        imageView = (ImageView) findViewById(R.id.imageView);
         btnprocesar = (Button) findViewById(R.id.btnprocesar);
+        btnfoto = (Button) findViewById(R.id.btnfoto);
+
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Permiso();
+            }
+        });
 
         btnprocesar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,nombres.getText().toString(),Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this,nombres.getText().toString(),Toast.LENGTH_LONG).show();
+//
+//                Intent intent = new Intent(MainActivity.this, activityResultActivity.class);
+//                startActivity(intent);
+//
+//                intent.putExtra("pnombres", nombres.getText().toString());
+//                intent.putExtra("papellidos", apellidos.getText().toString());
+//                intent.putExtra("pedad", edad.getText().toString());
 
-                Intent intent = new Intent(MainActivity.this, activityResultActivity.class);
-                startActivity(intent);
-
-                intent.putExtra("pnombres", nombres.getText().toString());
-                intent.putExtra("papellidos", apellidos.getText().toString());
-                intent.putExtra("pedad", edad.getText().toString());
-
+                AddPersonas();
+                
 
             }
         });
 
+    }
+
+    private void Permiso()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, PERMISO_CAMARA);
+        }
+        else
+        {
+            OpenCamara();
+        }
+    }
+
+    private void OpenCamara()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        tomarFotoLauncher.launch(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+
+        if(requestCode == PERMISO_CAMARA)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                OpenCamara();
+            }
+            else
+            {
+                Toast.makeText(this, "Permiso de camara denegado", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void AddPersonas()
+    {
+        try {
+            SQLiteConexion conexion = new SQLiteConexion(this, Transacciones.DBNAME, null, 1);
+            SQLiteDatabase db = conexion.getWritableDatabase();
+            ContentValues valores = new ContentValues();
+            valores.put(Transacciones.nombres, nombres.getText().toString());
+            valores.put(Transacciones.apellidos, apellidos.getText().toString());
+            valores.put(Transacciones.edad, Integer.parseInt(edad.getText().toString()));
+            valores.put(Transacciones.correo, correo.getText().toString());
+            valores.put(Transacciones.foto, nombres.getText().toString());
+
+            long resultado = db.insert(Transacciones.TABLENAME, null, valores);
+            db.close();
+
+            if(resultado > 0)
+            {
+                Toast.makeText(this, "Registro guardado " + resultado , Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(this, "Error Registro no guardado " + resultado , Toast.LENGTH_LONG).show();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
